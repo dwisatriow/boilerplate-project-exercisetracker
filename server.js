1d59e04192e17d790b096e65464ffac8913e370a
dwisatriow <dwisatrioow@gmail.com>
2021-10-03 00:56:15 +0700

    Install mongoose and setup db connection

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+// setup server
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
+});
+
+// setup mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// init mongodb connection
+mongoose.connect(process.env["MONGO_URI"], {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// checking connection
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Database connected");
 });
 
 
@@ -16,3 +34,4 @@ app.get('/', (req, res) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+;
