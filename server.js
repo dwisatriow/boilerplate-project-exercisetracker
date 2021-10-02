489efaf38950c0dc5f5b89b3fa1958d9672be07b
dwisatriow <dwisatrioow@gmail.com>
2021-10-03 01:05:59 +0700

    Setup body parser and add request logger

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const bodyParser = require("body-parser");
 
+// setup cors and body parser
 app.use(cors());
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// request logger
+app.use(function (req, res, next) {
+  var log = req.method + " " + req.path + " - " + req.ip;
+  console.log(log);
+  next();
 });
 
 // setup mongoose
@@ -27,10 +34,18 @@ db.once("open", function () {
   console.log("Database connected");
 });
 
+// serve public as static
+app.use(express.static('public'));
+
+// serve index page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
 
 
 
 
+// listen to port
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
