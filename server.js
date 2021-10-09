fd7b9a45c941d876e0bf8bb7fe41d3da6363f2fb
dwisatriow <dwisatrioow@gmail.com>
2021-10-09 22:49:14 +0700

    Add api to create new user and get list of all users

--- server.js
+++ server.js
@@ -1,51 +1,80 @@
-// setup server
+// Setup server
 const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const bodyParser = require("body-parser");
 
-// setup cors and body parser
+// Setup cors and body parser
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// request logger
+// Request logger
 app.use(function (req, res, next) {
   var log = req.method + " " + req.path + " - " + req.ip;
   console.log(log);
   next();
 });
 
-// setup mongoose
+// Setup mongoose
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-// init mongodb connection
+// Init mongodb connection
 mongoose.connect(process.env["MONGO_URI"], {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// checking connection
+// Checking connection
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
   console.log("Database connected");
 });
 
-// serve public as static
+// Serve public as static
 app.use(express.static('public'));
 
-// serve index page
+// Serve index page
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// Setup mongoose schema and model
+const userSchema = new Schema({
+  username: String,
+});
 
+const User = mongoose.model("User", userSchema);
 
+// Create new user api
+app.post("/api/users", function (req, res) {
+  const username = req.body.username;
 
-// listen to port
+  const newUser = new User({
+    username: username,
+  });
+
+  newUser.save(function (err, newUser) {
+    if (err) return console.error(err);
+    res.json({
+      username: newUser.username,
+      _id: newUser.id,
+    })
+  });
+});
+
+// Get all users api
+app.get("/api/users", function (req, res) {
+  User.find({}, "username id", function (err, docs) {
+    if (err) return console.error(err);
+    res.json(docs);
+  });
+});
+
+// Listen to port
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
